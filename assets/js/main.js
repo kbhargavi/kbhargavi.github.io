d02bed72e61061e34ce74ff202aa817082273f17
Brett Clanton <brettclanton001@gmail.com>
2015-03-15 21:08:33 -0400

    start on projects grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,19 @@ $(function() {
     $('.header-area').height(windowHeight);
   }
 
+  function verticalAlignProjectInfo(){
+    $('.project').each( function(index, element){
+      var $element = $(element);
+      var $inner = $element.find('.inner');
+      $inner.css({
+        paddingTop: ($element.height() - $inner.height()) * 0.5
+      });
+    });
+  }
+
   // ON PAGE LOAD
   adjustHeaderHeight();
+  verticalAlignProjectInfo();
 
   // EVENT LISTENERS
   $( window ).resize(function(){
